24f102e0a4ef06d98a4d9601f02b0a861d0e23db
sheltowt <sheltowt@gmail.com>
2016-04-12 14:58:38 -0700

    testing .gitignore

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
-import main  
+package main  
 
 import (
+	"log"
 	"github.com/spf13/viper"
 )
 
 func main() {
 	viper.SetConfigName("example_config")
-	
+	viper.AddConfigPath("$GOPATH/src/github.com/crunchbase_data")
+	viper.SetConfigType("json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+
 }
